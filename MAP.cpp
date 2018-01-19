2cab8443409e1b7335f3053be7789afbc6d22a98
Lab User <user@wi.zut.pl>
2018-01-19 21:14:34 +0100

    Changed the procedural program in to the objectiv

--- MAP.cpp
+++ MAP.cpp
@@ -2,24 +2,45 @@
 
 using namespace std;
 
-int map[10][10];
-
-int main(int argc, char* argv[])
+class Map
 {
-    for (int i = 0; i < 10; ++i)
+    int map[12][12];
+
+public:
+    Map()
     {
-        for (int j = 0; j < 10; ++j)
+        for (int i = 0; i < 12; ++i)
         {
-            if (j == 10-1)
+            for (int j = 0; j < 12; ++j)
             {
-                cout << '_'<<'|'<<endl;
-            }
-            else
-            {
-                cout << '_';
+                map[i][j] = 0;
             }
         }
     }
+
+    void DrawMap()
+    {
+        for (int i = 0; i < 12; ++i)
+        {
+            for (int j = 0; j < 12; ++j)
+            {
+                if (j == 12-1)
+                {
+                    cout << map [i][j] <<'|'<<endl;
+                }
+                else
+                {
+                    cout << map[i][j];
+                }
+            }
+        }
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Map M1;
+    M1.DrawMap();
 
     return 0;
 }
