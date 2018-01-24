0e01a2e881ad3af8358a332bbec15c51d2bd43c3
Dorota <ananeke@gmail.com>
2018-01-24 09:21:26 +0100

    Next ideas

--- Room.cpp
+++ Room.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <array>
 #include <algorithm>
+#include <ctime>
 
 std::array<std::string, 6> thing {"Sword","Armor","Boots","Ring","Shield","Gloves"};
 std::array<std::string, 3> good_prefix {"Good","Magic","Legendary"};
@@ -14,13 +15,10 @@ class Stuff
 public:
     Stuff()
     {
-        name = std::random_shuffle (thing, thing + sizeof(thing)/sizeof(thing[0]));
-    }
-
-    void Print()
-    {
-
-        std::cout<<name<<std::endl;
+        //randomowe wybieranie nagrody
+        srand(time(NULL));
+        int index = rand() % 6;
+        this->name = thing[index];
     }
 };
 
@@ -28,14 +26,14 @@ class Room
 {
     bool room_open = true;
     bool road_open = false;
-    std::string loot = Stuff();
+
 };
 
 
 int main(int argc, char const *argv[]) {
 
     Stuff S;
-    S.Print();
+    //S.Print();
 
     for(const auto &v: thing)
         std::cout << v << " ";
